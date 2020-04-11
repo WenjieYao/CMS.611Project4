6656b2260bc05ab8ad7b406b6485f7a3ea24591f
Jessica Liu <jvliu@mit.edu>
2020-04-11 02:50:20 -0400

    Added EnemyMovement scene with basic enemy-goes-to-player and spawning movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Enemy's attacking target
+	[SerializeField]
+	private GameObject target = null;
+
+	// Current rigidbody2d object
+    private Rigidbody2D rigidbody2D;
+    //Enemy movement speed
+    [SerializeField]
+    private float speed = 2;
+
+	public GameObject Target
+	{
+		get{
+			return target;
+		}
+		set
+		{
+			this.target = value;
+		}
+	}
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    	Vector2 moveDirection = (Vector2)(target.transform.position - transform.position);
+    	rigidbody2D.MovePosition(rigidbody2D.position + moveDirection * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+
+	/************Properties*************/
+
+	//Time to wait before next spawn
+	[SerializeField] 
+	private float spawnTime = 1;
+	
+	[SerializeField]
+	private GameObject enemyPrefab = null;
+
+	[SerializeField]
+	private int enemiesPerSpawn = 1;
+
+	//target that we want instantiated enemy to follow
+	[SerializeField]
+	private GameObject target = null;
+
+	public float SpawnRate{get; set;}
+
+	public GameObject EnemyPrefab{get; set;}
+
+	public int EnemiesPerSpawn{get; set;}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	if (target == null)
+            target = GameObject.FindGameObjectWithTag("Player");
+        
+        StartCoroutine(SpawnEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*Spawn numEnemies enemies around the spawn point and set their targets.*/
+    IEnumerator SpawnEnemy(){
+    	while (true){
+    		yield return new WaitForSeconds(spawnTime);
+
+	        for (int i = 0; i < enemiesPerSpawn; i++)
+	        {
+	            // Create a new enemy 
+	            Debug.Log("here");
+	            GameObject newEnemy = Instantiate(enemyPrefab, (Vector2)transform.position, Quaternion.identity);
+	            // Set enemy target
+	            Debug.Log(newEnemy);
+
+	            newEnemy.GetComponent<Enemy>().Target = target;
+	        }
+    	}
+    }
+}
