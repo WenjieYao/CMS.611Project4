12ba099c4dd454d490331660951156ff7ea347cf
Daniel Liu <hi.daniel.liu@gmail.com>
2020-04-21 12:21:00 -0700

    added funcitonality to all start menu buttons

---
+++ Assets/Scripts/MenuPanelManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanelManager : MonoBehaviour
+{
+    #region Inspector Variables
+
+    public GameObject BlackScreen;
+    public GameObject HelpPopup;
+    public GameObject CreditsPopup;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlackScreen.SetActive(true);
+        HelpPopup.SetActive(false);
+        CreditsPopup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenPanel(int idx)
+    {
+        switch (idx)
+        {
+            case 1:
+                HelpPopup.SetActive(true);
+                break;
+            case 2:
+                CreditsPopup.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void ClosePanel(int idx)
+    {
+        switch (idx)
+        {
+            case 1:
+                HelpPopup.SetActive(false);
+                break;
+            case 2:
+                CreditsPopup.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
